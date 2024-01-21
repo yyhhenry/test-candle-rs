f6c2bf786e75c3cd4fdfbdc73d11e000c22434fb
yyhhenry <yyhhenry@foxmail.com>
2024-01-21 13:39:54 +0800

    Refactor training script and add linear model

--- src/bin/train.rs
+++ src/bin/train.rs
@@ -1,25 +1,73 @@
-use candle_core::{DType, Device, IndexOp, ModuleT, Result};
+use candle_core::{DType, Device, IndexOp, Result, Tensor};
 use candle_datasets::vision::mnist;
-use candle_nn::{loss::cross_entropy, optim::AdamW, Optimizer, VarBuilder, VarMap};
+use candle_nn::{loss::cross_entropy, optim::AdamW, Optimizer, ParamsAdamW, VarBuilder, VarMap};
 use clap::Parser;
 use indicatif::ProgressIterator;
-use test_candle_rs::CNNModel;
+use test_candle_rs::{CNNModel, LinearModel, MNISTModel};
 
 #[derive(Parser)]
 struct Args {
-    #[clap(long, default_value = "1000")]
-    batch_size: usize,
-    #[clap(long, default_value = "10")]
-    epochs: usize,
-    #[clap(long, default_value = "0.001")]
+    #[arg(long)]
+    batch_size: Option<usize>,
+    #[arg(long)]
+    epochs: Option<usize>,
+    #[arg(long, default_value = "0.001")]
     lr: f64,
+    #[arg(short, long)]
+    model_path: Option<String>,
+    #[arg(long)]
+    linear: bool,
 }
 
-fn batch_progress(size: usize, batch_size: usize) -> impl Iterator<Item = (usize, usize)> {
-    (0..size)
-        .step_by(batch_size)
-        .progress()
-        .map(move |start| (start, (start + batch_size).min(size)))
+struct MNISTDatasetSplit {
+    images: Tensor,
+    labels: Tensor,
+    size: usize,
+}
+impl MNISTDatasetSplit {
+    pub fn new(images: Tensor, labels: Tensor) -> Self {
+        let size = images.shape().dims()[0];
+        Self {
+            images,
+            labels,
+            size,
+        }
+    }
+    pub fn i(&self, start: usize, end: usize) -> Result<Self> {
+        Ok(Self::new(
+            self.images.i(start..end)?,
+            self.labels.i(start..end)?,
+        ))
+    }
+    pub fn batch(&self, batch_size: usize) -> impl Iterator<Item = Result<Self>> + '_ {
+        (0..self.size)
+            .step_by(batch_size)
+            .map(move |start| self.i(start, (start + batch_size).min(self.size)))
+    }
+}
+fn load_dataset(device: &Device) -> Result<(MNISTDatasetSplit, MNISTDatasetSplit)> {
+    let dataset = mnist::load()?;
+
+    let train_images = dataset
+        .train_images
+        .to_dtype(DType::F32)?
+        .to_device(&device)?;
+    let train_labels = dataset
+        .train_labels
+        .to_dtype(DType::U32)?
+        .to_device(&device)?;
+    let train = MNISTDatasetSplit::new(train_images, train_labels);
+
+    let test_images = dataset
+        .test_images
+        .to_dtype(DType::F32)?
+        .to_device(&device)?;
+    let test_labels = dataset
+        .test_labels
+        .to_dtype(DType::U32)?
+        .to_device(&device)?;
+    let test = MNISTDatasetSplit::new(test_images, test_labels);
+    Ok((train, test))
 }
 
 fn main() -> Result<()> {
@@ -27,56 +75,58 @@ fn main() -> Result<()> {
 
     // Initialize
     let args = Args::parse();
+    let batch_size = args
+        .batch_size
+        .unwrap_or(if args.linear { 3000 } else { 600 });
+    let model_path = args.model_path.unwrap_or(format!(
+        "./model/{}_model.safetensors",
+        if args.linear { "linear" } else { "cnn" }
+    ));
+    let epochs = args.epochs.unwrap_or(if args.linear { 50 } else { 10 });
     let device = Device::cuda_if_available(0)?;
     device.set_seed(42)?;
 
     // Load dataset
-    let dataset = mnist::load()?;
-
-    let train_images = dataset
-        .train_images
-        .to_dtype(DType::F32)?
-        .reshape(((), 1, 28, 28))?
-        .to_device(&device)?;
-    let train_labels = dataset
-        .train_labels
-        .to_dtype(DType::U32)?
-        .to_device(&device)?;
-    let train_size = train_images.shape().dims()[0];
-
-    let test_images = dataset
-        .test_images
-        .to_dtype(DType::F32)?
-        .reshape(((), 1, 28, 28))?
-        .to_device(&device)?;
-    let test_labels = dataset
-        .test_labels
-        .to_dtype(DType::U32)?
-        .to_device(&device)?;
-    let test_size = test_images.shape().dims()[0];
+    let (train_set, test_set) = load_dataset(&device)?;
 
     println!("Dataset loaded.");
 
     // Build model
     let vm = VarMap::new();
     let vb = VarBuilder::from_varmap(&vm, DType::F32, &device);
-    let model = CNNModel::new(vb)?;
-    let mut opt = AdamW::new_lr(vm.all_vars(), args.lr)?;
+    let model: Box<dyn MNISTModel> = if args.linear {
+        Box::new(LinearModel::new(vb)?)
+    } else {
+        Box::new(CNNModel::new(vb)?)
+    };
+    let mut opt = AdamW::new(
+        vm.all_vars(),
+        ParamsAdamW {
+            lr: args.lr,
+            weight_decay: 0.,
+            ..Default::default()
+        },
+    )?;
     println!("Model built.");
 
-    for epoch in 1..=args.epochs {
+    for epoch in 1..=epochs {
         let start_time = std::time::Instant::now();
         println!("Epoch {} training", epoch);
-        let mut loss_sum = 0.0;
-        for (start, end) in batch_progress(train_size, args.batch_size) {
-            let images = train_images.i(start..end)?;
-            let labels = train_labels.i(start..end)?;
-            let output = model.forward_t(&images, true)?;
-            let loss = cross_entropy(&output, &labels)?;
+        let mut loss_sum = Tensor::new(0.0_f32, &device)?;
+        for entry in train_set
+            .batch(batch_size)
+            .try_progress()
+            .expect("Failed to show progress bar")
+        {
+            let d = entry?;
+            let output = model.forward_t(&d.images, true)?;
+            let loss = cross_entropy(&output, &d.labels)?;
             opt.backward_step(&loss)?;
-            loss_sum += loss.to_scalar::<f32>()? * (end - start) as f32;
+            let loss = loss.detach()?.affine(d.size as f64, 0.0)?;
+            loss_sum = loss_sum.add(&loss)?;
         }
-        let avg_loss = loss_sum / train_size as f32;
+        let loss_sum = loss_sum.to_scalar::<f32>()?;
+        let avg_loss = loss_sum / train_set.size as f32;
         println!(
             "\tavg_loss: {:8.5} (Cost {:.3}s)",
             avg_loss,
@@ -85,24 +135,28 @@ fn main() -> Result<()> {
 
         let start_time = std::time::Instant::now();
         println!("Epoch {} evaluating", epoch);
-        let mut correct_sum = 0;
-        for (start, end) in batch_progress(test_size, args.batch_size) {
-            let images = test_images.i(start..end)?;
-            let labels = test_labels.i(start..end)?;
-            let output = model.predict_argmax(&images)?;
-            let correct = output.eq(&labels)?.to_dtype(DType::U32)?.sum(0)?;
-            correct_sum += correct.to_scalar::<u32>()?;
+        let mut correct_sum = Tensor::new(0_u32, &device)?;
+        for entry in test_set
+            .batch(batch_size)
+            .try_progress()
+            .expect("Failed to show progress bar")
+        {
+            let d = entry?;
+            let output = model.predict_argmax(&d.images)?;
+            let correct = output.eq(&d.labels)?.to_dtype(DType::U32)?.sum(0)?;
+            correct_sum = correct_sum.add(&correct.detach()?)?;
         }
-        let accuracy = correct_sum as f32 / test_size as f32;
+        let correct_sum = correct_sum.to_scalar::<u32>()?;
+        let accuracy = correct_sum as f32 / test_set.size as f32;
         println!(
             "\taccuracy: {:5} / {:5} = {:8.3}% (Cost {:.3}s)",
             correct_sum,
-            test_size,
+            test_set.size,
             accuracy * 100.0,
             start_time.elapsed().as_secs_f32()
         );
     }
-    vm.save("./model/model.safetensors")?;
+    vm.save(model_path)?;
     println!("Model saved.");
     println!(
         "Total time: {:.3}s",
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
-use candle_core::{ModuleT, Result, Tensor};
+use candle_core::{DType, ModuleT, Result, Tensor};
 use candle_nn as nn;
+pub trait MNISTModel: ModuleT {
+    fn predict_softmax(&self, xs: &Tensor) -> Result<Tensor> {
+        let output = self.forward_t(xs, false)?;
+        Ok(nn::ops::softmax(&output, 1)?)
+    }
+    fn predict_argmax(&self, xs: &Tensor) -> Result<Tensor> {
+        Ok(self.predict_softmax(xs)?.argmax(1)?)
+    }
+    fn predict_one_argmax(&self, x: &Tensor) -> Result<(Vec<f32>, usize)> {
+        let xs = x.reshape((1, ()))?;
+        let output = self.predict_softmax(&xs)?;
+        let argmax = output.argmax(1)?.to_dtype(DType::I64)?.to_scalar::<i64>()?;
+        Ok((output.to_vec1()?, argmax as usize))
+    }
+}
 struct ConvBlock {
     conv: nn::Conv2d,
     bn: nn::BatchNorm,
@@ -56,13 +71,11 @@ impl CNNModel {
             fc2,
         })
     }
-    pub fn predict_argmax(&self, xs: &Tensor) -> Result<Tensor> {
-        Ok(self.forward_t(xs, false)?.argmax(1)?)
-    }
 }
 impl nn::ModuleT for CNNModel {
     fn forward_t(&self, xs: &Tensor, train: bool) -> Result<Tensor> {
         let xs = xs
+            .reshape(((), 1, 28, 28))?
             .apply_t(&self.conv1, train)?
             .apply_t(&self.conv2, train)?
             .reshape(((), 16 * 24 * 24))?
@@ -73,3 +86,28 @@ impl nn::ModuleT for CNNModel {
         Ok(xs)
     }
 }
+impl MNISTModel for CNNModel {}
+
+pub struct LinearModel {
+    fc1: nn::Linear,
+    fc2: nn::Linear,
+}
+
+impl LinearModel {
+    pub fn new(vb: nn::VarBuilder) -> Result<Self> {
+        let fc1 = nn::linear(28 * 28, 128, vb.pp("fc1"))?;
+        let fc2 = nn::linear(128, 10, vb.pp("fc2"))?;
+        Ok(Self { fc1, fc2 })
+    }
+}
+impl nn::Module for LinearModel {
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        let xs = xs
+            .reshape(((), 28 * 28))?
+            .apply(&self.fc1)?
+            .relu()?
+            .apply(&self.fc2)?;
+        Ok(xs)
+    }
+}
+impl MNISTModel for LinearModel {}
